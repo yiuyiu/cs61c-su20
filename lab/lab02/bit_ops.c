569ded368408a44b3456794576be045f64e56c09
xixi <capriciousfish@outlook.com>
2022-12-28 16:24:12 +0800

    finish lab2 excercise 0 1

--- lab/lab02/bit_ops.c
+++ lab/lab02/bit_ops.c
@@ -9,6 +9,8 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns
     // 0 or 1)
+    unsigned mask = x >> n;
+    return 1 && mask;
     return -1;
 }
 // Set the nth bit of the value of x to v.
@@ -17,11 +19,22 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+        // Create a mask with the nth bit set to 1 and the rest set to 0
+    unsigned mask = 1 << n;
+
+    // Set the nth bit to 0
+    *x &= ~mask;
+
+    // Set the nth bit to v
+    *x |= (v << n);
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    unsigned value = get_bit(*x, n);
+    unsigned res = value ^ 1;
+    set_bit(x, n, res);
 }
 
