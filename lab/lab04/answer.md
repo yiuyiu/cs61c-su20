7c403a52d2c0119ef79fde0006af066f9fe13fc4
xixi <yu.yi@trip.com>
2023-02-10 20:24:42 +0800

    lab4 exercise 1 finished

--- lab/lab04/answer.md
+++ lab/lab04/answer.md
@@ -1,5 +1,8 @@
 1. line 78 lw a1, 0(s1) 就直接mv s1 a1就行了，它a1是地址，不是要取值
 2. 用t0计数会有问题，因为line 71 jalr s1调用后t0可能会发生变化，在调用后依然是用了t0. t1,t2也是同理.
 3. 它是先执行再判断的，如果数组长度是0的话就有问题了。
-4. line 77 la a0, 8(s0)语法错误，没有这种写法就直接mv s0 a0就行了
-
+4. line 77 la a0, 8(s0)语法错误，没有这种写法就直接mv a0 s0就行了
+5. load the value at that address into a0这一步是将node的array的地址放入a0，而不是array里的第一个值，因为node里的array是array地址
+6. offset the array address by the count有问题，
+t0是1，2，3，4这样的数字
+t1存放的是array里每一项的地址
--- lab/lab04/megalistmanips.s
+++ lab/lab04/megalistmanips.s
@@ -64,18 +64,28 @@ map:
 mapLoop:
     add t1, s0, x0      # load the address of the array of current node into t1
     lw t2, 4(s0)        # load the size of the node's array into t2
+    
+    lw t1, 0(t1)        # 加载array的地址
+    addi t4, x0, 4          
+    mul t4 t4 t0        # 计算实际偏移量
+    add t1, t1, t4      # offset the array address by the count
 
-    add t1, t1, t0      # offset the array address by the count
     lw a0, 0(t1)        # load the value at that address into a0
-
+    addi sp, sp, -12
+    sw t0, 0(sp)
+    sw t1, 4(sp)
+    sw t2, 8(sp)
     jalr s1             # call the function on that value.
-
+    lw t0, 0(sp)
+    lw t1, 4(sp)
+    lw t2, 8(sp)
+    addi sp, sp, 12
     sw a0, 0(t1)        # store the returned value back into the array
     addi t0, t0, 1      # increment the count
     bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
-
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+    addi a0 s0 8      # load the address of the next node into a0
+    lw a0 0(a0)
+    mv a1 s1      # put the address of the function back into a1 to prepare for the recursion
 
     jal  map            # recurse
 done:
